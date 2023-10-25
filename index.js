8d0b07a2bb8445ab2e6a020c3241a27ad734bdcf
shivarhombuz <shiva@rhombuz.io>
2023-10-25 20:24:53 +0530

    install, use packages and start the server

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const {Server} = require('socket.io');
+const http = require('http');
+const port = 8000;
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors:{
+        origin:'*',
+        mathods:["GET", "POST"]
+    }
+});
+
+//3.use socket event
+io.on('connection', (socket)=>{
+    console.log('Connection Is Established');
+
+    socket.on('disconnect', ()=>{
+        console.log('Connection Is Disconnected');
+    })
+})
+
+server.listen(port,function(err){
+    if(err){
+        console.log(`Error while server is up: ${err}`);
+        return;
+    }
+    console.log(`Server is up and running on port: ${port}`);
+})
